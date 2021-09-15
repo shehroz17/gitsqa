5d82498ef5bfb33276f42a0e48fcac30225e71f6
shehroz ahmar <shehrozahmar17@gmail.com>
2021-09-15 19:31:59 +0500

    okay

---
+++ shehroz.py
@@ -0,0 +1,12 @@
+print ('hello world')
+print ('what is your name?')
+my_name = input()
+
+print("My name is " + my_name)
+print (len(my_name))
+
+print ("my age is")
+my_age = input()
+print("my age is "+  my_age)
+
+print ("you will be " + str(int(my_age) + 1 ) + " in a year")
